7fc525bfbb777770aa7ca2a9a26b674f7017c75a
William Fong <trigition@gmail.com>
2016-04-04 10:26:56 -0600

    Voxels now set their domain to wool blocks.

--- src/com/SkyIsland/DynamicDungeons/Voxel/Voxel.java
+++ src/com/SkyIsland/DynamicDungeons/Voxel/Voxel.java
@@ -1,5 +1,11 @@
 package com.SkyIsland.DynamicDungeons.Voxel;
 
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
 /**
  * This class represents a base block unit of a {@link DungeonNode} <br />
  * and is used for structural representation and construction.
@@ -8,11 +14,18 @@ package com.SkyIsland.DynamicDungeons.Voxel;
  */
 public class Voxel {
 	
+	private static final int max_X = 5;
+	private static final int max_Y = 5;
+	private static final int max_z = 5;
+	
 	private int voxel_X;
 	private int voxel_Y;
 	private int voxel_Z;
 	
 	private Voxel prev_Voxel;
+	private World world;
+	private Location vLocation;
+	
 	
 	/**
 	 * This constructor creates a {@link Voxel} instance at 0,0,0. This is the base voxel <br />
@@ -34,14 +47,31 @@ public class Voxel {
 	 * @param rel_y The relative y coordinate from the base.
 	 * @param rel_z The relative z coordinate from the base.
 	 */
-	public Voxel(Voxel v, int rel_x, int rel_y, int rel_z) {
+	public Voxel(Voxel v, World w, int rel_x, int rel_y, int rel_z) {
 		this.voxel_X = v.getVoxel_X() + rel_x;
 		this.voxel_Y = v.getVoxel_Y() + rel_y;
 		this.voxel_Z = v.getVoxel_Z() + rel_z;
 		
 		this.prev_Voxel = v;
+		
+		this.world = w;
+		this.vLocation = new Location(w, this.voxel_X, this.voxel_Y, this.voxel_Z);
 	}
-
+	
+	public void construct() {
+		Chunk buildChunk = this.vLocation.getChunk();
+		Block block;
+		// Build in order of ZXY
+		for (int x = 0; x < max_X; x++) {
+			for (int y = 0; y < max_Y; y++) {
+				for (int z = 0; z < max_z; z++) {
+					//TODO construct block with specified material
+					block = buildChunk.getBlock(x, y, z);
+					block.setType(Material.WOOL);
+				}
+			}
+		}
+	}
 	
 	// Getters
 	
